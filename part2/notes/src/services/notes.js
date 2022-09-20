79d321c89dd66d92d6ee90e5cf71fe189e2bbb5f
Rahul Desar <desarrahul@gmail.com>
2022-09-20 13:57:17 +0545

    completed till 5.4

--- part2/notes/src/services/notes.js
+++ part2/notes/src/services/notes.js
@@ -1,14 +1,25 @@
-import axios from 'axios'
-const baseUrl = '/api/notes'
+import axios from 'axios';
+const baseUrl = '/api/notes';
+
+let token = null;
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`;
+}
 
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
+const create = async newObject => {
+  const config = {
+    headers : { Authorization : token },
+  }
+  console.log(baseUrl, newObject, config);
+  const response = await axios.post(baseUrl, newObject, config)
+  console.log(response);
+  return response.data;
 }
 
 const update = (id, newObject) => {
@@ -16,8 +27,4 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
-export default { 
-  getAll: getAll, 
-  create: create, 
-  update: update 
-}
+export default { getAll, create, update, setToken};
