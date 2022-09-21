13b418a0c55b2d77cdcc137e73a981c2f15d2706
Rahul Desar <desarrahul@gmail.com>
2022-09-21 12:24:10 +0545

    some lint error correction

--- part2/notes/src/components/NoteForm.js
+++ part2/notes/src/components/NoteForm.js
@@ -1,22 +1,22 @@
 import { useState } from 'react';
 
-const NoteForm = ({createNote}) => {
+const NoteForm = ({ createNote }) => {
   // onSubmit, handleChange, value
-  const [newNote, setNewNote] = useState('') 
+  const [newNote, setNewNote] = useState('');
 
   const handleChange = (event) => {
-    setNewNote(event.target.value)
-  }
+    setNewNote(event.target.value);
+  };
 
   const addNote = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     createNote({
       content: newNote,
       important: Math.random() > 0.5,
-    })
+    });
 
-    setNewNote('')
-  }
+    setNewNote('');
+  };
   return (
     <div>
       <h2>Create a new note</h2>
@@ -29,7 +29,7 @@ const NoteForm = ({createNote}) => {
         <button type="submit">save</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default NoteForm;
--- part2/notes/src/services/notes.js
+++ part2/notes/src/services/notes.js
@@ -5,24 +5,24 @@ let token = null;
 
 const setToken = newToken => {
   token = `bearer ${newToken}`;
-}
+};
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then(response => response.data);
+};
 
 const create = async newObject => {
   const config = {
     headers : { Authorization : token },
-  }
-  const response = await axios.post(baseUrl, newObject, config)
+  };
+  const response = await axios.post(baseUrl, newObject, config);
   return response.data;
-}
+};
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
-}
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then(response => response.data);
+};
 
-export default { getAll, create, update, setToken};
+export default { getAll, create, update, setToken };
