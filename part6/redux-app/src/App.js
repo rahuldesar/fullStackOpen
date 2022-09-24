63ece24c217a8fb4f42057a040c0a7909c6ba1cd
Rahul Desar <desarrahul@gmail.com>
2022-09-24 16:06:36 +0545

    unicafe start and note redux app

--- part6/redux-app/src/App.js
+++ part6/redux-app/src/App.js
@@ -1,8 +1,73 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { createStore } from 'redux';
+
+// whole state of the application is stored into one JavaScript-object in the store
+
+const action = {
+  type: 'INCREMENT',
+  payload: '',
+}
+
+// state of the application is defined using a reducer
+
+const somethingReducer = (state , action) => {
+  switch(action.type){
+    case 'INCREMENT':
+      return state + 1;
+    default:
+      return state;
+    }
+  
+
+}
+
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    case 'ZERO':
+      return 0;
+    default: // if none of the above matches, code comes here
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+// store.subscribe(() => {
+//   const storeNow = store.getState()
+//   console.log(storeNow)
+// })
+
+
+
 const App = () => {
   return (
     <div>
-      YO
+    <div>
+      {store.getState()}
     </div>
+    <button 
+      onClick={e => store.dispatch({ type: 'INCREMENT' })}
+    >
+      plus
+    </button>
+    <button
+      onClick={e => store.dispatch({ type: 'DECREMENT' })}
+    >
+      minus
+    </button>
+    <button 
+      onClick={e => store.dispatch({ type: 'ZERO' })}
+    >
+      zero
+    </button>
+  </div>
   );
 }
 
--- part6/redux-app/src/index.js
+++ part6/redux-app/src/index.js
@@ -1,5 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import { createStore } from 'redux';
+import noteReducer from './reducers/noteReducer';
+
+
+const store = createStore(noteReducer)
+store.subscribe(() => {
+  const storeNow = store.getState();
+  console.log(storeNow);
+})
+
+store.dispatch({
+  type:'NEW_NOTE',
+  data:{
+    content : 'ADDED BY REDUX REDUCER',
+    important: false,
+    id:2,
+  }
+});
+store.dispatch({
+  type:'NEW_NOTE',
+  data:{
+    content : 'ADDED BY REDUX REDUCER2',
+    important: true,
+    id:3,
+  }
+});
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        {store.getState().map(note => 
+          <li key={ note.id }>
+            { note.content } <strong>{ note.important? 'important' : '' }</strong>
+          </li>
+        )}
+      </ul>
+      <button onClick={
+        e =>store.dispatch({
+          type:'NEW_NOTE',
+          data: {
+            content: 'testing button',
+            important: true,
+            id : Math.random()*100,
+          }  
+        }
+      ) }> ADD NEW NOTE </button>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const renderApp = () => root.render(<App />)
+
+renderApp()
+store.subscribe(renderApp);
