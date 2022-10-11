3e746aae7fb7fbd35399d7e52a61842ec894663d
Rahul Desar <desarrahul@gmail.com>
2022-10-11 19:53:33 +0545

    some linting

--- part9- TypeScript/ts-intro/multiplier.ts
+++ part9- TypeScript/ts-intro/multiplier.ts
@@ -30,7 +30,7 @@ try{
 } catch (error: unknown) {
   let errorMessage = 'Something bad happened';
   if(error instanceof Error){
-    errorMessage += 'Error: ' + error.message;
+    errorMessage += ' Error: ' + error.message;
   }
   console.log(errorMessage);
 }
