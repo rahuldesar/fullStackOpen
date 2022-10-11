0ce71319984c44933d3ba9f8bf96c09e703beccb
Rahul Desar <desarrahul@gmail.com>
2022-10-11 18:35:56 +0545

    multiply for CLI

--- part9- TypeScript/ts-intro/multiplier.ts
+++ part9- TypeScript/ts-intro/multiplier.ts
@@ -1,14 +1,36 @@
 
-const multiplicator = (a: number, b: number, printText: string) : string => {
-  return `${printText}, ${a*b}`;
+
+interface MultiplyValues { 
+  value1: number;
+  value2: number;
 }
 
-console.log(typeof(process.argv[2]));
-console.log(typeof(process.argv[3]));
-const a : number = Number(process.argv[2]);
-const b : number = Number(process.argv[3]);
+const parseArguments = (args: Array<string>): MultiplyValues => {
+  if(args.length < 4 ) throw new Error('Not enough Arguments ');
+  if(args.length > 4 ) throw new Error('Too many Arguments');
 
+  if(!isNaN(Number(args[2])) && !isNaN(Number(args[3]))){
+    return{
+      value1: Number(args[2]),
+      value2: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not Numbers');
+  }
 
-console.log(multiplicator(a, b, `Multiplied numbers ${a} and ${b}, the result is:`));
+}
 
+const multiplicator = (a: number, b: number, printText: string) => {
+  console.log(printText, a*b);
+}
 
+try{
+  const{value1 , value2 } = parseArguments(process.argv);
+  multiplicator(value1, value2, `Multiplied ${value1} and ${value2}, the result is : `);
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened';
+  if(error instanceof Error){
+    errorMessage += 'Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
